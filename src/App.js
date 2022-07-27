b56d476ea0e1c6d54592d013dca1c657b33c56e7
amcnulty <amcnulty88@swbell.net>
2022-07-27 09:54:44 -0700

    Media Query and Install Button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const deferredPrompt = useRef();
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+        deferredPrompt.current = e;
+    });
+
+    const handleInstallClick = () => {
+        if (deferredPrompt.current !== null) {
+            deferredPrompt.current.prompt();
+            deferredPrompt.current.userChoice.then((outcome) => {
+                if (outcome === 'accepted') {
+                    deferredPrompt = null;
+                }
+            });
+        }
+    };
+
+    return (
+        <div className='App'>
+            <div className='installSection'>
+                <button onClick={handleInstallClick}>Install App</button>
+            </div>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <p>The new content</p>
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
